b4f1de4750288bc798d3189a0748be9e9c2a03e7
John Nguyen <ipwnponies@gmail.com>
2021-03-28 17:55:32 -0700

    Add youtube clear playlist script
    Boilerplate for adding button to clear youtube playlist.

---
+++ greasemonkey/youtube-clear-playlist.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name        Clear Youtube playlists
+// @namespace   ipwnponies
+// @match       https://www.youtube.com/playlist
+// @grant       none
+// @version     0.1
+// @description Add a button to clear all items in a playlist.
+// ==/UserScript==
