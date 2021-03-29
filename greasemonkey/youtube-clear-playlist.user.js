cb90d3379afa5b29140cf52261ac8cd9651709ba
John Nguyen <ipwnponies@gmail.com>
2021-03-28 22:52:28 -0700

    Add mutation observer callback filter
    Target the relevant node with filter. Stub addRemoveAllButton, which
    will be used to implement the change.

--- greasemonkey/youtube-clear-playlist.user.js
+++ greasemonkey/youtube-clear-playlist.user.js
@@ -6,12 +6,25 @@
 // @version     0.1
 // @description Add a button to clear all items in a playlist.
 // ==/UserScript==
+const addRemoveAllButton = (container) => {
+};
+
+const mutationCallback = (mutationsList, observer) => {
+  // Find mutation that results in populating sibling nodes
+  // This means it's safe for us to append our node
+  const mutation = mutationsList.find(
+    ({ target, addedNodes }) => target.elementMatches('tp-yt-paper-listbox#items') && addedNodes.length,
+  );
+
+  if (mutation) {
+    observer.disconnect();
+    addRemoveAllButton(mutation.target);
+  }
+};
+
 window.addEventListener('DOMContentLoaded', async () => {
   const frame = document.querySelector('ytd-popup-container');
 
   const observer = new MutationObserver(mutationCallback);
   observer.observe(frame, { subtree: true, childList: true });
 });
-
-const mutationCallback = (mutationsList, observer) => {
-};
