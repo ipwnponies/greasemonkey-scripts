abdf57b1538ac0adcce45a273bd0514d63927d90
John Nguyen <ipwnponies@gmail.com>
2021-03-28 19:23:07 -0700

    Add mutation observer on playlist menu
    This is the menu that contains other playlist management options.
    The elements are not populated until clicked. Listening on this will
    give the appropriate time to add a new menu item.

--- greasemonkey/youtube-clear-playlist.user.js
+++ greasemonkey/youtube-clear-playlist.user.js
@@ -6,3 +6,12 @@
 // @version     0.1
 // @description Add a button to clear all items in a playlist.
 // ==/UserScript==
+window.addEventListener('DOMContentLoaded', async () => {
+  const frame = document.querySelector('ytd-popup-container');
+
+  const observer = new MutationObserver(mutationCallback);
+  observer.observe(frame, { subtree: true, childList: true });
+});
+
+const mutationCallback = (mutationsList, observer) => {
+};
