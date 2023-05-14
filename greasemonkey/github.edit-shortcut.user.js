c6c11eb2583a2cc6b323a531160ac6116fc71bb7
John Nguyen <ipwnponies@gmail.com>
2023-05-14 00:43:25 -0700

    Add github edit shortcut script

---
+++ greasemonkey/github.edit-shortcut.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name        Edit shortcut - github.com
+// @namespace   ipwnponies
+// @match       https://github.com/*/pull/*
+// @grant       none
+// @version     1.0
+// @author      ipwnponies
+// @require     https://cdn.jsdelivr.net/npm/@violentmonkey/shortcut@1
+// @description Add shortcut to edit the pull request description
+// ==/UserScript==
+const { register } = VM.shortcut;
+
+register('ctrl-k', () => {
+  const issueCommentEdit = document.querySelector(
+    '.editable-comment[id^=pullrequest-] .timeline-comment-action.js-comment-edit-button',
+  );
+  issueCommentEdit.click();
+});
