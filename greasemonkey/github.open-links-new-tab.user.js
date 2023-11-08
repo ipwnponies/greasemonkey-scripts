6b34f889d4240167249562ea42633fd91839099a
John Nguyen <ipwnponies@gmail.com>
2023-11-07 16:52:26 -0800

    Fix github.open-links-new-tab to better handle SPA
    Use the mutation observer API to continually ensure all links have blank
    target.
    This can be replaced with the Navigation API, which allows adding event
    listener on href changes.

--- greasemonkey/github.open-links-new-tab.user.js
+++ greasemonkey/github.open-links-new-tab.user.js
@@ -3,9 +3,16 @@
 // @namespace   ipwnponies
 // @match       https://github.com/*/*/pull/*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      ipwnponies
 // @description When you're reviewing a github pull request, the last thing you want is to navigate away from that tab.
 // ==/UserScript==
-const links = document.querySelectorAll('.comment-body a');
-links.forEach((i) => (i.target = '_blank'));
+
+const mutationCallback = () => {
+  const links = document.querySelectorAll('.comment-body a');
+
+  links.forEach((i) => (i.target = '_blank'));
+};
+
+const observer = new MutationObserver(mutationCallback);
+observer.observe(document.querySelector('body'), { subtree: true, childList: true });
