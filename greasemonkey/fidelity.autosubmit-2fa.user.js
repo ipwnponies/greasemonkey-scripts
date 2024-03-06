70455e0942400576a1cae82ae1193934a60c10ec
John Nguyen <ipwnponies@gmail.com>
2024-03-06 13:56:45 -0800

    Update fidelity to auto click login and 2fa button
    Monitor for change events, which is triggered by password manager.

--- greasemonkey/fidelity.autosubmit-2fa.user.js
+++ greasemonkey/fidelity.autosubmit-2fa.user.js
@@ -3,20 +3,42 @@
 // @namespace   ipwnponies
 // @match       https://digital.fidelity.com/prgw/digital/login/full-page*
 // @grant       none
-// @version     1.0
+// @version     2.0
 // @author      ipwnponies
 // @require https://cdn.jsdelivr.net/npm/@violentmonkey/dom@2
-// @description Clicky button when 2fa code is pasted
+// @description Clicky button when login or 2fa code is pasted or inserted by password manager
 // ==/UserScript==
 
+// Login
+const getLoginButton = () => document.querySelector('#dom-login-button');
 VM.observe(document.querySelector('body'), () => {
-  const totpInput = document.querySelector('#dom-svip-security-code-input');
+  const totpInput = getLoginButton();
   if (totpInput) {
-    totpInput.addEventListener('paste', () => {
+    totpInput.addEventListener('change', () => {
       setTimeout(() => {
-        document.querySelector('#dom-svip-code-submit-button').click();
-      }, 1);
+        getLoginButton().click();
+      });
     });
+
+    return true;
+  }
+
+  return undefined;
+});
+
+// 2FA
+const get2FAButton = () => document.querySelector('#dom-svip-code-submit-button');
+VM.observe(document.querySelector('body'), () => {
+  const totpInput = get2FAButton();
+  if (totpInput) {
+    const clickyButton = () => {
+      setTimeout(() => {
+        get2FAButton().click();
+      }, 1);
+    };
+    totpInput.addEventListener('paste', clickyButton);
+    // password manager integration is a field change event
+    totpInput.addEventListener('change', clickyButton);
 
     return true;
   }
