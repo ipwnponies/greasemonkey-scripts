d3de2516725bdec3e038e6f46ae0ea548fea3721
John Nguyen <ipwnponies@gmail.com>
2024-03-08 12:00:11 -0800

    Fix github open links script to only target external links
    Only apply target blank to non-anchor links. This lets anchors continue
    working normally.

--- greasemonkey/github.open-links-new-tab.user.js
+++ greasemonkey/github.open-links-new-tab.user.js
@@ -3,15 +3,20 @@
 // @namespace   ipwnponies
 // @match       https://github.com/*/*/pull/*
 // @grant       none
-// @version     1.1
+// @version     1.2
 // @author      ipwnponies
 // @description When you're reviewing a github pull request, the last thing you want is to navigate away from that tab.
 // ==/UserScript==
-
 const mutationCallback = () => {
+  const isAnchorLink = (origin, pathname) => origin == location.origin && pathname == location.pathname;
   const links = document.querySelectorAll('.comment-body a');
 
-  links.forEach((i) => (i.target = '_blank'));
+  // Leave anchor links alone. target="_blank" breaks their anchorness.
+  const externalLinks = Array.from(links).filter((i) => !isAnchorLink(i.origin, i.pathname));
+
+  externalLinks.forEach((i) => {
+    i.target = '_blank';
+  });
 };
 
 const observer = new MutationObserver(mutationCallback);
