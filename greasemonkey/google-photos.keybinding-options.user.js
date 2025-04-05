61d4f80894fdbe5b37de4cbeeb51697c2dde5b1e
John Nguyen <ipwnponies@gmail.com>
2025-04-05 16:05:28 -0700

    Add google photos keybinding to access options menu
    There's no clean CSS selector. Otherwise this would have been a
    tridactyl binding.

---
+++ greasemonkey/google-photos.keybinding-options.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name        Keybinding for options -  photos.google.com
+// @namespace   ipwnponies
+// @match       https://photos.google.com/share/
+// @match       https://photos.google.com/album/
+// @grant       none
+// @version     1.0
+// @author      ipwnponies
+// @require https://cdn.jsdelivr.net/npm/@violentmonkey/shortcut@1
+// @description Keybinding to access the options menu in Google Photos
+// ==/UserScript==
+
+const { register } = VM.shortcut;
+
+register('period', () => {
+  const elements = document.querySelectorAll('div[aria-label="More options"]');
+  const element = Array.from(elements).find((i) => i.checkVisibility());
+
+  element.click();
+});
