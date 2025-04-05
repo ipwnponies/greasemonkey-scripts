c0bf77dfa3b9f4e35789f178fb50694a25bed1b7
John Nguyen <ipwnponies@gmail.com>
2025-04-05 16:38:10 -0700

    Add usbank autosubmit

---
+++ greasemonkey/usbank.autosubmit-2fa.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name        Autosubmit 2FA script usbank.com
+// @namespace   ipwnponies
+// @match       https://onlinebanking.usbank.com/auth/login/*
+// @grant       none
+// @version     1.0
+// @author      ipwnponies
+// @require     https://cdn.jsdelivr.net/npm/@violentmonkey/dom@2
+// @description Clicky button when login or 2fa code is pasted or inserted by password manager
+// ==/UserScript==
+
+VM.observe(document.querySelector('body'), () => {
+  const form = document.querySelector('#otpEntryPageForm');
+
+  if (form) {
+    const input = form.querySelector('input');
+    const submit = form.querySelector('button[type="submit"]');
+
+    for (const event of ['paste', 'change']) {
+      input.addEventListener(event, () => {
+        requestAnimationFrame(() => {
+          submit.click();
+        });
+      });
+    }
+
+    return true;
+  }
+
+  return undefined;
+});
