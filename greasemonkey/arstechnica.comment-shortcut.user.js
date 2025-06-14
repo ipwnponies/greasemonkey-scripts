a2aad4d73b4b9e8a1a8e1e2079e1fec3404ad14e
John Nguyen <ipwnponies@gmail.com>
2025-06-14 01:13:59 -0700

    Update arstechnica comment script to sort by vote count
    Extend the comment hotkey.
    After loading comments, also sort by vote.

--- greasemonkey/arstechnica.comment-shortcut.user.js
+++ greasemonkey/arstechnica.comment-shortcut.user.js
@@ -3,9 +3,9 @@
 // @namespace   ipwnponies
 // @match       https://arstechnica.com/*
 // @grant       none
-// @version     1.0
+// @version     2.0
 // @author      ipwnponies
-// @require https://cdn.jsdelivr.net/npm/@violentmonkey/shortcut@1
+// @require     https://cdn.jsdelivr.net/npm/@violentmonkey/shortcut@1
 // @description Add shortcut to go to comment section
 // ==/UserScript==
 
@@ -14,6 +14,12 @@ const { register } = VM.shortcut;
 // There are several comment bubbles, any of them work
 const comments = document.querySelector('a.view-comments');
 
-register('ctrl-k', () => {
+register('ctrl-k', async () => {
   comments.click();
+  await new Promise((r) => setTimeout(r, 500));
+
+  const commentFrame = document.querySelector('#xf_thread_iframe');
+  const url = new URL(commentFrame.contentDocument.location);
+  url.searchParams.set('order', 'vote_score');
+  commentFrame.contentDocument.location = url;
 });
