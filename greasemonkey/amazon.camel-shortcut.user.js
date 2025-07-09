7fc4511d33e63324dde0c67a7b68e571c136a679
John Nguyen <ipwnponies@gmail.com>
2025-07-09 13:01:43 -0700

    Add additional url for amazon shortcut

--- greasemonkey/amazon.camel-shortcut.user.js
+++ greasemonkey/amazon.camel-shortcut.user.js
@@ -3,8 +3,9 @@
 // @namespace   ipwnponies
 // @match       https://www.amazon.com/dp/*
 // @match       https://www.amazon.com/*/dp/*
+// @match       https://www.amazon.com/gp/product/*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @require     https://cdn.jsdelivr.net/npm/@violentmonkey/shortcut@1
 // @description Add shortcut to look up item in camelcamelcamel
 // ==/UserScript==
@@ -13,7 +14,11 @@ const { register } = VM.shortcut;
 
 register('ctrl-k', () => {
   const url = new URL(location);
-  const match = url.pathname.match(/\/dp\/(?<product>[A-Z0-9]+)/i);
+
+  const regexp = url.pathname.startsWith('/gp/product/')
+    ? /\/gp\/product\/(?<product>[A-Z0-9]+)/i
+    : /\/dp\/(?<product>[A-Z0-9]+)/i;
+  const match = url.pathname.match(regexp);
   const { product } = match.groups;
 
   location.assign(`https://camelcamelcamel.com/product/${product}`);
