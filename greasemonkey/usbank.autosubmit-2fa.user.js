f063b2a567849525b165a144cd18db071c6900d3
John Nguyen <ipwnponies@gmail.com>
2025-07-22 10:41:43 -0700

    Update css selector for target DOM element for usbank script

--- greasemonkey/usbank.autosubmit-2fa.user.js
+++ greasemonkey/usbank.autosubmit-2fa.user.js
@@ -3,7 +3,7 @@
 // @namespace   ipwnponies
 // @match       https://onlinebanking.usbank.com/auth/login/*
 // @grant       none
-// @version     1.0
+// @version     1.0.1
 // @author      ipwnponies
 // @require     https://cdn.jsdelivr.net/npm/@violentmonkey/dom@2
 // @description Clicky button when login or 2fa code is pasted or inserted by password manager
@@ -14,7 +14,7 @@ VM.observe(document.querySelector('body'), () => {
 
   if (form) {
     const input = form.querySelector('input');
-    const submit = form.querySelector('button[type="submit"]');
+    const submit = form.querySelector('#otp-cont-button');
 
     for (const event of ['paste', 'change']) {
       input.addEventListener(event, () => {
