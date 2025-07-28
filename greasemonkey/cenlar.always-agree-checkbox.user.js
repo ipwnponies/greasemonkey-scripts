91d7d8260e108a4f2b4b5eecf2200ae47c135ad6
John Nguyen <ipwnponies@gmail.com>
2025-07-28 13:53:57 -0700

    Fix the cenlar url match
    The landing page redirects.

--- greasemonkey/cenlar.always-agree-checkbox.user.js
+++ greasemonkey/cenlar.always-agree-checkbox.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name        Always check agree box - loanadministration.com
 // @author      ipwnponies
-// @version     1.1
-// @match       https://www.loanadministration.com/cenlarfsb/
+// @version     1.1.1
+// @match       https://www.loanadministration.com/*
 // @grant       none
 // @require     https://cdn.jsdelivr.net/npm/@violentmonkey/dom@2
 // @description I'm already logging in, why wouldn't I agree to the terms?
