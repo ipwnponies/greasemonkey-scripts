0bf73c80e6f5365ec88ae5e4ab01a5b02534308d
John Nguyen <ipwnponies@gmail.com>
2025-08-14 11:12:17 -0700

    Update duckduckgo css selector
    They updated their UI

--- greasemonkey/duckduckgo.just-get-to-the-page-already.user.js
+++ greasemonkey/duckduckgo.just-get-to-the-page-already.user.js
@@ -3,14 +3,16 @@
 // @namespace   ipwnponies
 // @match       https://duckduckgo.com/*
 // @grant       none
-// @version     1.1
+// @version     1.2
 // @author      ipwnponies
 // @require     https://cdn.jsdelivr.net/npm/@violentmonkey/dom@2
 // @description Clicky button when using AI chat. It always prompts because no prior cookies when using clean containers
 // ==/UserScript==
 
 VM.observe(document.querySelector('body'), () => {
-  const agreeButton = Array.from(document.querySelectorAll('main button')).find((i) => i.textContent == 'I Agree');
+  const agreeButton = Array.from(document.querySelectorAll('[data-type=modal-overlay] button')).find(
+    (i) => i.textContent === 'Agree and Continue',
+  );
   if (agreeButton) {
     agreeButton.click();
     return true;
