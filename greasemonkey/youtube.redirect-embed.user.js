cda0d6789baa9fc211ec5a363bb7dfde6104ac1e
John Nguyen <ipwnponies@gmail.com>
2025-12-22 14:51:34 -0800

    Fix youtube script to handle video id as part of url path

--- greasemonkey/youtube.redirect-embed.user.js
+++ greasemonkey/youtube.redirect-embed.user.js
@@ -2,17 +2,42 @@
 // @name        Redirect youtube to nocoookie - youtube.com
 // @namespace   ipwnponies
 // @match       https://www.youtube.com/embed/watch?v=*
+// @match       https://www.youtube.com/embed/*
 // @match       https://www.youtube.com/watch?v=*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @description Redirect from youtube to nocookie embed Add referrer to youtube embed
 // ==/UserScript==
 
-function redirect() {
-  'use strict';
+'use strict';
 
-  const searchParams = new URLSearchParams(location.search);
-  const videoId = searchParams.get('v');
+function resolveVideoId(search, pathname) {
+  const searchParams = new URLSearchParams(search);
+  const fromQuery = searchParams.get('v');
+  if (fromQuery) {
+    return fromQuery;
+  }
+
+  // Handle URLs like https://www.youtube.com/embed/<VIDEO_ID> when the query is empty
+  const parts = pathname.split('/').filter(Boolean);
+  const embedIndex = parts.indexOf('embed');
+  if (embedIndex !== -1) {
+    const potentialId = parts[embedIndex + 1];
+    if (potentialId && potentialId !== 'watch') {
+      return potentialId;
+    }
+  }
+
+  return null;
+}
+
+function redirect() {
+  const videoId = resolveVideoId(location.search, location.pathname);
+  if (!videoId) {
+    console.warn('Unable to determine YouTube video id for redirect', location.href);
+    return;
+  }
+
   const redirectUrl = new URL(`https://www.youtube-nocookie.com/embed/${videoId}`);
   window.location.assign(redirectUrl);
 }
