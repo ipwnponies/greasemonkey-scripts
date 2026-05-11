d17f685768da13a484a2e57234c8da3541b73f46
John Nguyen <ipwnponies@gmail.com>
2026-05-11 13:28:13 -0700

    refactor(userscript): Replace polling loop with VM.observe, fix beforebegin case

--- greasemonkey/leetcode-filtering.user.js
+++ greasemonkey/leetcode-filtering.user.js
@@ -3,74 +3,32 @@
 // @namespace   ipwnponies
 // @match       https://leetcode.com/tag/*
 // @grant       none
-// @version     1.0.1
+// @version     1.0.2
+// @require     https://cdn.jsdelivr.net/npm/@violentmonkey/dom@2
 // @description When viewing problems by tags, there are no filters.
-//              Why? Who knows. But it's a trivial problem solved with some javscripts.
 // ==/UserScript==
 
 const filters = new Map([
-  [
-    'easy',
-    {
-      text: 'Easy',
-      id: 'easyCheckboxId',
-      cssSelector: 'span.label-success',
-    },
-  ],
-  [
-    'medium',
-    {
-      text: 'Medium',
-      id: 'medCheckboxId',
-      cssSelector: 'span.label-warning',
-    },
-  ],
-  [
-    'hard',
-    {
-      text: 'Hard',
-      id: 'hardCheckboxId',
-      cssSelector: 'span.label-danger',
-    },
-  ],
-  [
-    'completed',
-    {
-      text: 'Completed',
-      id: 'completedCheckboxId',
-      cssSelector: '.text-success',
-    },
-  ],
-  [
-    'subscription',
-    {
-      text: 'Subscription Required',
-      id: 'subscriptionCheckboxId',
-      cssSelector: '.fa-lock',
-    },
-  ],
+  ['easy', { text: 'Easy', id: 'easyCheckboxId', cssSelector: 'span.label-success' }],
+  ['medium', { text: 'Medium', id: 'medCheckboxId', cssSelector: 'span.label-warning' }],
+  ['hard', { text: 'Hard', id: 'hardCheckboxId', cssSelector: 'span.label-danger' }],
+  ['completed', { text: 'Completed', id: 'completedCheckboxId', cssSelector: '.text-success' }],
+  ['subscription', { text: 'Subscription Required', id: 'subscriptionCheckboxId', cssSelector: '.fa-lock' }],
 ]);
 
 const filterTests = () => {
-  const allTests = Array.from(document.querySelectorAll('.reactable-data > tr').values());
-  allTests.forEach((i) => {
-    i.hidden = false;
-  });
+  const allTests = Array.from(document.querySelectorAll('.reactable-data > tr'));
+  allTests.forEach((i) => { i.hidden = false; });
 
   for (const { id, cssSelector } of filters.values()) {
     const { checked } = document.getElementById(id);
     if (!checked) {
-      allTests
-        .filter((i) => i.querySelector(cssSelector))
-        .forEach((i) => {
-          i.hidden = !checked;
-        });
+      allTests.filter((i) => i.querySelector(cssSelector)).forEach((i) => { i.hidden = true; });
     }
   }
 };
 
 const createFilter = ({ text, id }) => {
-  // I was too lazy to introduce a parent component, so that I could probably add left-right margins
   const checkbox = document.createElement('input');
   checkbox.type = 'checkbox';
   checkbox.id = id;
@@ -83,23 +41,18 @@ const createFilter = ({ text, id }) => {
   label.htmlFor = id;
   label.style.marginRight = '1em';
 
-  document.querySelector('.table-responsive').insertAdjacentElement('beforeBegin', checkbox);
-  document.querySelector('.table-responsive').insertAdjacentElement('beforeBegin', label);
+  const table = document.querySelector('.table-responsive');
+  table.insertAdjacentElement('beforebegin', checkbox);
+  table.insertAdjacentElement('beforebegin', label);
 };
 
-async function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-async function main() {
-  while (!document.querySelector('.table-responsive')) {
-    // eslint-disable-next-line no-await-in-loop
-    await sleep(200);
+VM.observe(document.body, () => {
+  if (document.querySelector('.table-responsive')) {
+    for (const filter of filters.values()) {
+      createFilter(filter);
+    }
+    return true;
   }
 
-  for (const i of filters.values()) {
-    createFilter(i);
-  }
-}
-
-main();
+  return undefined;
+});
