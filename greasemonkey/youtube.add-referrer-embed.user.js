29a40fb6cbbd2f8de7854d1d29d14238342baf8b
John Nguyen <ipwnponies@gmail.com>
2026-06-20 00:47:44 -0700

    Fix ESLint errors in neetcode and youtube scripts

--- greasemonkey/youtube.add-referrer-embed.user.js
+++ greasemonkey/youtube.add-referrer-embed.user.js
@@ -3,11 +3,9 @@
 // @namespace   ipwnponies
 // @match       https://www.youtube-nocookie.com/embed/*
 // @grant       none
-// @version     1.0.2
+// @version     1.0.3
 // @description Add referrer to youtube embed. So that it works. Incredible
 // ==/UserScript==
-
-'use strict';
 
 function buildRedirectorUrl(targetUrl) {
   const redirector = new URL('https://duckduckgo.com/l/');
